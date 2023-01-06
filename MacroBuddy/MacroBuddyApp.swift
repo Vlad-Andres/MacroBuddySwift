b0d2732d2a282f9789aecad9a92a981c5c49e770
Vlad-Andres <bruno993409@gmail.com>
2023-01-06 16:59:45 +0200

    Initial Commit

---
+++ MacroBuddy/MacroBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroBuddyApp.swift
+//  MacroBuddy
+//
+//  Created by Vlad Andres on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MacroBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
